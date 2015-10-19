0fd364959da50086ec1b272b0a8837d855015936
SORN <sergio.roldan@gft.com>
2015-10-19 18:49:27 +0200

    remove imports

--- src/main/java/com/sample/client/WebConfig.java
+++ src/main/java/com/sample/client/WebConfig.java
@@ -3,7 +3,6 @@ package com.sample.client;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;

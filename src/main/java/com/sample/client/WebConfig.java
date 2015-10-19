34ea946cd486dc186119693bd07cc227caacb244
SORN <sergio.roldan@gft.com>
2015-10-19 18:49:15 +0200

    loggers

--- src/main/java/com/sample/client/WebConfig.java
+++ src/main/java/com/sample/client/WebConfig.java
@@ -3,6 +3,7 @@ package com.sample.client;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,19 +13,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Slf4j
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Value("${logging.file}")
-	private String loggingFile;
+	@Value("${bar}")
+	private String bar;
 
 	@RequestMapping("/test")
 	@ResponseBody
 	public String user() {
 		
 		log.debug("debug");
-		log.debug("info");
-		log.debug("warn");
-		log.debug("error");
+		log.info("info");
+		log.warn("warn");
+		log.error("error");
 		
-		return loggingFile;
+		return bar;
 	}
 
 }
